aa8f062c9c94cefbf3229f2fea686719d4a2fd76
Omrika <omrikafolkes@gmail.com>
2015-11-11 17:43:16 -0500

    shows msg onclick

---
+++ granny.js
@@ -0,0 +1,11 @@
+var grandma = document.getElementById("grandma")
+grandma.addEventListener("click", myFunction);
+
+function myFunction() {
+    grandma.innerHTML = "YOU CLICKED ME!";   
+}
+
+//when i click grandma i want her to move right until she's close enoough to grandchild to hear 
+//when she reaches a certain point then the she yell's the right phrase to grandma 
+//and then the game is over.
+
